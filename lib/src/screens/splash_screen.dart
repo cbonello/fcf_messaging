17f8f3eca3c732fdebc25f5b045e6787bc2f5376
Christophe Bonello <cbonello@users.noreply.github.com>
2020-02-02 23:25:45 -0500

    update splash screen with loading animation

--- lib/src/screens/splash_screen.dart
+++ lib/src/screens/splash_screen.dart
@@ -1,8 +1,60 @@
+import 'package:fcf_messaging/src/services/app_localizations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  AssetImage logo;
+
+  @override
+  void initState() {
+    logo = const AssetImage('assets/images/splash_logo.png');
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    precacheImage(logo, context);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Splash Screen')));
+    final Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FractionallySizedBox(
+              widthFactor: 0.5,
+              child: Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Image(image: logo),
+                  SpinKitThreeBounce(
+                    color: Colors.white,
+                    size: size.width * 0.5 * 0.3,
+                  ),
+                ],
+              ),
+            ),
+            AnimatedOpacity(
+              opacity: 1.0,
+              duration: const Duration(seconds: 1),
+              child: Text(
+                context.l10n().appTitle,
+                style: Theme.of(context).textTheme.title,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
